ffeb60a4dc1df656f77ab178244593d9f16decbd
Jainam <85880788+JAINAM576@users.noreply.github.com>
2025-03-16 17:50:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://medium.com")
 print(driver.page_source)
 
